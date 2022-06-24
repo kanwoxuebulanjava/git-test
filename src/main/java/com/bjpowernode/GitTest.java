4cdbd0268f0589e983e54123016bef017a32abb7
root <2783650388@qq.com>
2022-06-24 16:40:46 +0800

    master commit1

--- src/main/java/com/bjpowernode/GitTest.java
+++ src/main/java/com/bjpowernode/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("liwang yuexin san wan yuan ren min bi");
         System.out.println("heloo git1");
         System.out.println("ming tian mian shi");
+        System.out.println("liwang haohao xuexi");
     }
 }
