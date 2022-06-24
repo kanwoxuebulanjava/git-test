123412a66be0e02e5c250b7480cfb81362988204
root <2783650388@qq.com>
2022-06-24 20:55:32 +0800

    dqaw

--- src/main/java/com/bjpowernode/Test.java
+++ src/main/java/com/bjpowernode/Test.java
@@ -3,5 +3,7 @@ package com.bjpowernode;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello".equals("liwang"));
+        System.out.println("hello".equals("liwang"));
+        System.out.println("hello".equals("liwang"));
     }
 }
